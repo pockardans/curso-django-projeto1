7be74628d5c90c6092e5de56f3a352bdc3feeb3c
Paulo de Oliveira Campos Jr <paulo.oc.jr@gmail.com>
2025-08-05 14:14:40 -0300

    aula 22

---
+++ recipes/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):       
+    return HttpResponse ('HOME')
+
+def sobre(request):
+    return HttpResponse ('SOBRE')
+
+def contato(request):
+    return HttpResponse ('CONTATO')
+
